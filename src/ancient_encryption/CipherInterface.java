c378c153410972b6669c9a68e83ace630a590f18
Emil Tang Kristensen <emiltangkristensen@gmail.com>
2017-02-18 20:08:08 +0100

    Never mind fixed again

--- src/ancient_encryption/CipherInterface.java
+++ src/ancient_encryption/CipherInterface.java
@@ -9,9 +9,9 @@ package ancient_encryption;
 public interface CipherInterface {
 
     char[] ALPHABETH = {'a', 'A', 'b', 'B', 'c', 'C', 'd', 'D', 'e', 'E', 'f', 'F',
-	 'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M',
-	 'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
-	 'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'};
+	'g', 'G', 'h', 'H', 'i', 'I', 'j', 'J', 'k', 'K', 'l', 'L', 'm', 'M',
+	'n', 'N', 'o', 'O', 'p', 'P', 'q', 'Q', 'r', 'R', 's', 'S', 't', 'T',
+	'u', 'U', 'v', 'V', 'w', 'W', 'x', 'X', 'y', 'Y', 'z', 'Z'};
 
     String encrypt(String original);
 
